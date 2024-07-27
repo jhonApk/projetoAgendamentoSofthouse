6c2da8796f9207ef85ab18fd39f607302bd1e3e1
jhonApk <devjhon.mobile@gmail.com>
2024-07-26 23:43:41 -0300

    ui em desenvolvimento

---
+++ app/src/main/java/com/example/projectagendamento/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.projectagendamento
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.projectagendamento.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+
+    private fun initNavigation(){
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.btnv, navController )
+    }
+}
